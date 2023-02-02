e20c9823bc17059922b519d7a1300984726e4269
Kondrat <yo126@yandex.ru>
2023-02-02 23:10:56 +0300

    zadacha_3

--- zadacha3/Program.cs
+++ zadacha3/Program.cs
@@ -5,7 +5,10 @@
 System.Console.WriteLine("Введите N");
 int N = Convert.ToInt32(Console.ReadLine());
 
-for (int i=1; i<=N; i++)
+for (int i = 1; i < N; i++)
 {
-    System.Console.Write(Math.Pow((i),3)+" . ");
+    System.Console.Write(Math.Pow((i), 3) + ",");
+
 }
+
+System.Console.Write(Math.Pow((N), 3) + ".");
